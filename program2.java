89968c9c228e5bda333588283a1b2ac108d2ba73
mercygithub <99577568+mercygithub@users.noreply.github.com>
2022-02-13 11:39:28 +0530

    Create program2.java

---
+++ program2.java
@@ -0,0 +1,15 @@
+public class DisplayOddNumbers
+{
+  public static void main(String[]args)
+  {
+    int num=100;
+    system.out.println("list of odd numbers 1to"+num=".");
+    for(int i=1;i<=num;i++)
+    {
+      if(i%2!=0)
+      {
+        System.ou.println(i+"");
+      }
+    }
+  }
+}
