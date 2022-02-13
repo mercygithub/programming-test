26e6ad80eb21fbcb4285ab325c125b4aec0e4e34
mercygithub <99577568+mercygithub@users.noreply.github.com>
2022-02-13 12:20:29 +0530

    Create program4.java

---
+++ program4.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+class Count
+{
+  public static void main(String[]args)
+  {
+    int i,num,result;
+    System.out.println("Enter the numbe");
+    Scanner sc=new scanner(System.in);
+    num=sc.nextInt();
+    
